6c36e479f0aca65c65d9d4a9f1fe0eb3ddfe5683
Henrique Pereira de Souza <hrpsouza@outlook.com>
2022-08-24 19:52:42 -0300

    quinta mensagem de commit

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,14 @@
-nome = "HenriquePereiraDeSouza";
+nome = "Henrique Pereira de Souza";
 email = "hrpsouza@outlook.com";
-profissao = "bancario";
+profissao = "Bancário";
 idade = 50;
 destino = "NY";
+console.log("nome: " + nome);
+console.log("email: " + email);
+console.log("Profissão: " + profissao);
+console.log("idade: " + idade);
+console.log("destino: " + destino);
+console.log(" testando passar as 4 informações através de 1 única linha de código: ");
+console.log(" nome: " + nome, '\n', "email: " + email , '\n', "Profissão: " + profissao , '\n', "idade: " + idade , '\n',  "destino: " + destino );
+console.log(" transmitindo a frase própria da tarefa do dia: ");
+console.log("Olá," + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + destino + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D");
