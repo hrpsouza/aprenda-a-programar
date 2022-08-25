17d7b9265209bda7baa22194f95221c0535b4544
Henrique Pereira de Souza <hrpsouza@outlook.com>
2022-08-25 17:13:23 -0300

    decima mensagem de commit

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,7 @@
+let n = 9;
+let i = 1;
+console.log("Tabuada de " + n);
+while (i <= 10) {
+console.log(n + " x " + i + " = " + n*i);
+i = i + 1;
+}
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,7 @@
+let n = 1;
+while (n <= 16) {
+if (n%2 != 0) {
+console.log(n);
+}
+n = n + 1;
+}
